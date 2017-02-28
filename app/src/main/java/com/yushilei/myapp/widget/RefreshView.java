8e3fba7a60f4bb4b5787153ae6721035f64f0391
yushilei <582072387@qq.com>
2017-02-28 14:42:24 +0800

    1：简单完成下拉刷新 类似微信；

--- app/src/main/java/com/yushilei/myapp/widget/RefreshView.java
+++ app/src/main/java/com/yushilei/myapp/widget/RefreshView.java
@@ -1,10 +1,16 @@
 package com.yushilei.myapp.widget;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewConfiguration;
 import android.widget.FrameLayout;
+
+import com.yushilei.myapp.R;
 
 /**
  * @auther by yushilei.
@@ -14,7 +20,16 @@ import android.widget.FrameLayout;
 
 public class RefreshView extends FrameLayout {
 
-    private int touchSlop;
+    private static final String TAG = "RefreshView";
+
+    private static final float RotateRate = 0.6f;//旋转系数
+
+    private static final float SlideRate = 0.7f;//滑动系数
+
+    private View img;
+    private View header;
+    private View child;
+    private ObjectAnimator ani;
 
     public RefreshView(Context context) {
         super(context);
@@ -22,11 +37,33 @@ public class RefreshView extends FrameLayout {
 
     public RefreshView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        touchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        MarginLayoutParams lp = (MarginLayoutParams) header.getLayoutParams();
+        lp.topMargin = -header.getMeasuredHeight();
+        header.setLayoutParams(lp);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        if (getChildCount() > 2)
+            throw new RuntimeException("RefreshView最多容纳2个子View");
+        Log.d(TAG, "onFinishInflate");
+        header = findViewById(R.id.refresh_header);
+        img = header.findViewById(R.id.refresh_img);
+        child = getChildAt(0);
     }
 
     float mLastX;
     float mLastY;
+
+    boolean isDragged;
+    boolean isReleased;
+    boolean isOverEdge;
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
@@ -39,12 +76,15 @@ public class RefreshView extends FrameLayout {
             case MotionEvent.ACTION_MOVE:
                 float diffY = y - mLastY;
                 float diffX = x - mLastX;
-                if (refreshListener != null && diffY > 0 && diffY > Math.abs(diffX) && diffY >= touchSlop) {
+                if (refreshListener != null && diffY > 0 && diffY > Math.abs(diffX)) {
                     intercepted = refreshListener.canRefresh();
                 }
                 break;
             case MotionEvent.ACTION_UP:
                 break;
+        }
+        if (intercepted) {
+            isDragged = true;
         }
         mLastX = x;
         mLastY = y;
@@ -55,15 +95,74 @@ public class RefreshView extends FrameLayout {
     public boolean onTouchEvent(MotionEvent event) {
         float x = event.getX();
         float y = event.getY();
+
+        isOverEdge = child.getY() >= header.getHeight();
+
         switch (event.getAction()) {
-            case MotionEvent.ACTION_MOVE:
+            case MotionEvent.ACTION_MOVE: {
                 float diffY = y - mLastY;
-                if (getScrollY() - diffY <= 0) {
-                    scrollTo(0, (int) (getScrollY() - diffY));
+                if (child.getY() + diffY >= 0) {
+                    float newY = child.getY() + diffY * SlideRate;
+                    if (isOverEdge) {
+                        header.setY(0);
+                    } else {
+                        header.setY(newY - header.getHeight());
+                    }
+                    child.setY(newY);
+
+                    img.setRotation(img.getRotation() + diffY / RotateRate);
+                } else {
+                    child.setY(0);
                 }
-                break;
-            case MotionEvent.ACTION_UP:
-                break;
+            }
+            break;
+            case MotionEvent.ACTION_UP: {
+                isDragged = false;
+                isReleased = true;
+
+                if (isOverEdge) {//加载
+                    ani = ObjectAnimator.ofFloat(child, "Y", child.getY(), 0f);
+                    ani.setDuration((long) (child.getY() * 5));
+                    ani.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator animation) {
+
+                            float y1 = child.getY() - header.getHeight();
+
+                            if (isOverEdge) {
+                                img.setRotation(img.getRotation() - 10);
+                            } else {
+                                header.setY(y1);
+                            }
+                        }
+                    });
+                    ani.start();
+
+                } else {
+                    if (child.getY() <= 10) {
+                        child.setY(0);
+                        header.setY(-header.getHeight());
+                    } else {//弹性
+                        ani = ObjectAnimator.ofFloat(child, "Y", child.getY(), 0f);
+                        ani.setDuration((long) (child.getY() * 5));
+                        ani.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                            @Override
+                            public void onAnimationUpdate(ValueAnimator animation) {
+                                int bottom = header.getBottom();
+                                float y1 = child.getY() - header.getHeight();
+                                img.setRotation(img.getRotation() + (y1 - bottom) / RotateRate);
+                                if (isOverEdge) {
+
+                                } else {
+                                    header.setY(y1);
+                                }
+                            }
+                        });
+                        ani.start();
+                    }
+                }
+            }
+            break;
         }
         mLastX = x;
         mLastY = y;
