1b47353b6630eb852ebffc8e5d0e05e686376ab7
yushilei <582072387@qq.com>
2017-03-10 16:11:15 +0800

    1：Behavior 初步了解

--- app/src/main/java/com/yushilei/myapp/adapter/HideBehavior.java
+++ app/src/main/java/com/yushilei/myapp/adapter/HideBehavior.java
@@ -2,10 +2,13 @@ package com.yushilei.myapp.adapter;
 
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+
+import com.yushilei.myapp.R;
 
 /**
  * @auther by yushilei.
@@ -13,8 +16,10 @@ import android.view.View;
  * @desc
  */
 
-public class HideBehavior extends CoordinatorLayout.Behavior<View> {
+public class HideBehavior extends CoordinatorLayout.Behavior<RecyclerView> {
     private static final String TAG = "HideBehavior";
+
+    View dependency;
 
     public HideBehavior() {
     }
@@ -24,36 +29,43 @@ public class HideBehavior extends CoordinatorLayout.Behavior<View> {
     }
 
     @Override
-    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
-        return dependency instanceof RecyclerView;
+    public boolean layoutDependsOn(CoordinatorLayout parent, RecyclerView child, View dependency) {
+        Log.i(TAG, "layoutDependsOn");
+        boolean b = dependency.getId() == R.id.dependency;
+        if (b) {
+            this.dependency = dependency;
+        }
+        return b;
     }
 
     @Override
-    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
-        return super.onDependentViewChanged(parent, child, dependency);
-    }
-
-    @Override
-    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, View child, View directTargetChild, View target, int nestedScrollAxes) {
-        Log.i(TAG, "onStartNestedScroll");
+    public boolean onDependentViewChanged(CoordinatorLayout parent, RecyclerView child, View dependency) {
+        Log.i(TAG, "onDependentViewChanged");
+        int height = dependency.getHeight();
+        int bottom = (int) dependency.getY();
+        int i = height - bottom;
+        child.setY(height - 2 * i);
         return true;
     }
 
     @Override
-    public void onNestedScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
-        Log.i(TAG, "onNestedScroll=dxConsumed=" + dxConsumed + " dyConsumed" + dyConsumed);
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, RecyclerView child, View directTargetChild, View target, int nestedScrollAxes) {
+        Log.i(TAG, "onStartNestedScroll");
 
+        return ViewCompat.SCROLL_AXIS_VERTICAL == nestedScrollAxes;
+    }
+
+    @Override
+    public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, RecyclerView child, View target, int dx, int dy, int[] consumed) {
+        Log.i(TAG, "onNestedPreScroll dy=" + dy + " consumed[1]" + consumed[1] + " " + target);
+        consumed[1] = dy;
+        dependency.setY(dependency.getY() - dy);
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, RecyclerView child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        Log.i(TAG, "onNestedScroll");
         super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
     }
 
-    @Override
-    public boolean onNestedFling(CoordinatorLayout coordinatorLayout, View child, View target, float velocityX, float velocityY, boolean consumed) {
-        return super.onNestedFling(coordinatorLayout, child, target, velocityX, velocityY, consumed);
-    }
-
-    @Override
-    public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dx, int dy, int[] consumed) {
-        Log.i(TAG, "onNestedPreScroll dy" + dy + " consumed" + consumed[1]);
-        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed);
-    }
 }
