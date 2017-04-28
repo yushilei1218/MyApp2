7616f5f18844a18bd52072e20f10446defd1dffe
yushilei <582072387@qq.com>
2017-04-28 10:37:18 +0800

    1：测试下ObjectAnim

--- app/src/main/java/com/yushilei/myapp/ui/TestActivity.java
+++ app/src/main/java/com/yushilei/myapp/ui/TestActivity.java
@@ -2,6 +2,7 @@ package com.yushilei.myapp.ui;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -19,6 +20,7 @@ import android.util.Pair;
 import android.util.SparseArray;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ScrollView;
 import android.widget.Toast;
@@ -34,12 +36,17 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 public class TestActivity extends BaseActivity {
     @BindView(R.id.test_recycler)
     RecyclerView recyclerView;
     @BindView(R.id.test_lv)
     ListView lv;
+
+    @BindView(R.id.test_img)
+    ImageView img;
+    private ObjectAnimator animator;
 
     @Override
     public int getLayoutId() {
@@ -83,17 +90,52 @@ public class TestActivity extends BaseActivity {
             data.add("item+" + i);
         }
         lv.setAdapter(new ArrayAdapter<String>(this, R.layout.item_recycler, R.id.item_tv, data));
-
-        AnimatorListenerAdapter animatorListenerAdapter = new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-            }
-        };
         Pair<String, String> o = new Pair<>("a", "b");
         SparseArray<String> array = new SparseArray<>();
         array.append(1, "ac");
 
+
+    }
+
+    @OnClick(R.id.test_img)
+    void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.test_img:
+                startAnim();
+                break;
+        }
+    }
+
+    private boolean isAnim = false;
+
+    private void startAnim() {
+        if (isAnim) {
+            animator.cancel();
+        }
+        boolean selected = img.isSelected();
+
+        AnimatorListenerAdapter listener = new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                isAnim = true;
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                isAnim = false;
+            }
+        };
+        if (selected) {
+            img.setSelected(false);
+            animator = ObjectAnimator.ofFloat(img, "Rotation", 90, 0);
+
+        } else {
+            img.setSelected(true);
+            animator = ObjectAnimator.ofFloat(img, "Rotation", 0, 90);
+        }
+        animator.setDuration(1000);
+        animator.addListener(listener);
+        animator.start();
     }
 
     public void btnTest(View view) {
